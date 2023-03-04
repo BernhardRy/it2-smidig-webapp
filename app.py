fb3e421e8d4d7c6cc17b3f029ff35498bd8b318a
BernhardRy <bernhard.ry@gmail.com>
2023-03-04 17:37:01 +0100

    Startet å lage drink programmet og ryddet litt

--- app.py
+++ app.py
@@ -1,51 +1,12 @@
 from flask import Flask, render_template, request
-from r import hent_katte_bilde
+from r import hent_drinker
 import json
 
 app = Flask(__name__)
-
-# bruker = ""
-
-# fil = open("brukere.json")
-# brukere = json.load(fil)
-# fil.close()
 
 @app.route("/")
 def index():
     navn = "Sandvika"
     return render_template("index.html", navn=navn)
-
-@app.route("/katter")
-def katter():
-    tilfeldigKatt1 = hent_katte_bilde()
-    return render_template("katter.html", tilfeldigKatt1=tilfeldigKatt1)
-
-# @app.route("/registrer", methods=("POST", "GET"))
-# def rute_registrer():
-#     if request.methods == "POST":
-#         brukernavn = request.form["brukernavn"]
-#         passord = request.form["passord"]
-#         navn = request.form["navn"]
-
-#         if brukernavn == "" or passord == "" or navn == "":
-#             melding = "Fill in all forms"
-#             return render_template("registrer.html", melding=melding)
-#         elif brukernavn == brukere:
-#             melding = "The username is taken"
-#             return render_template("registrer.html", melding=melding)
-#         else:
-#             if brukernavn not in brukere:
-#                 brukere[brukernavn] = {
-#                     "navn": navn,
-#                     "passord": passord,
-#                     "bank": 20,
-#                     "kjopt": []
-#                 }
-#                 skriv_brukere()
-#                 return rute_logg_inn()
-#             else:
-#                 melding = "This username is taken"
-#                 return render_template("registrer.html", melding=melding)
-#     return render_template("registrer.html")
 
 app.run(debug=True)
--- r.py
+++ r.py
@@ -1,31 +1,49 @@
 import requests
-import json
+import random
 
-def hent_katte_bilde():
-    katteListe = []
-    # link = "https://api.thecatapi.com/v1/images/search?limit=1"
-    # o = requests.get(link)
-    fil = open("katte_poeng.json")
-    a = json.load(fil)
-    fil.close()
+drinker_usortert = []
+drinker_sortert = []
+ingredienser = []
+drinker_sortert_dic = {}
+for i in range(4):
+    link = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+    o = requests.get(link)
+    drinker_usortert.append(o.json())
+for i in drinker_usortert:
+    drinker_sortert.append(i["drinks"][0]["strDrink"])
+    midlertidig_ingredienser_drink = []
+    for a in range(15):
+        a += 1
+        if not i["drinks"][0]["strIngredient"+str(a)] == None:
+            midlertidig_ingredienser_drink.append(i["drinks"][0]["strIngredient"+str(a)])
+    drinker_sortert_dic[i["drinks"][0]["strDrink"]] = midlertidig_ingredienser_drink
+    ingredienser.append(midlertidig_ingredienser_drink)
+
+def hent_drinker():
+    drinkListe = list(drinker_sortert_dic.keys())
+    return drinker_sortert_dic
+
+def hent_ingredienser_til_drink():
+    drinkListe = list(drinker_sortert_dic.keys())
+    randomtall = random.randint(0, len(drinkListe))
+    randomDrink = drinkListe[randomtall]
+
+    return drinker_sortert_dic[randomDrink]
+
+def hent_riktig_drink():
+    # ingredienser = hent_ingredienser_til_drink()
+    return hent_drinker()
+
+def sjekk_vinn():
+    alle_drinker = hent_drinker()
+    print(alle_drinker)
+    gjett_drink = input(f"Hvilken drink tror du bruker ingrediensene {hent_ingredienser_til_drink()}?\n")
+    # if drinker_sortert_dic[gjett_drink] == hent_ingredienser_til_drink:
+    #     print(f"Riktig svar!")
+    # else:
+    #     print("Feil svar")
+    print(drinker_sortert_dic[gjett_drink])
     
-    for i in a["katter"]:
-        katteListe.append(a["katter"][i])
-
-    return katteListe
-
-# def write_json(new_data, filename='katte_poeng.json'):
-#     with open(filename,'r+') as fil:
-#         fil_data = json.load(fil)
-#         fil_data["katter"].append(new_data)
-#         fil.seek(0)
-#         json.dump(fil_data, fil, indent = 4)
-
-# for i in range(5):
-#     for a in hent_katte_bilde():
-#         dictionary = {}
-#         dictionary[] = {
-#             "bilde": a,
-#             "poeng": 0
-#         }
-#     write_json(dictionary)
+    
+# print(hent_drinker())
+sjekk_vinn()
