5ad84177f8d2aa9aff45f7a467bf594b6d4307dc
BernhardRy <bernhard.ry@gmail.com>
2023-03-30 12:46:19 +0200

    Kopling mellom drink_program og html

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, request
-from r import hent_drinker
+from drink_program import Drinks
 import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    navn = "Sandvika"
-    return render_template("index.html", navn=navn)
+    drinks = Drinks()
+    alle_drinker = drinks.hent_drinker()
+    hent_ingredienser = drinks.hent_ingredienser_til_drink()
+    riktig_drink = drinks.hent_riktig_drink()
+    bilder = drinks.hent_bilder()
+
+    return render_template("index.html", alle_drinker=alle_drinker, riktig_drink=riktig_drink, hent_ingredienser=hent_ingredienser, bilder=bilder)
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    projectpath = request.form['projectFilepath']
+    return render_template("index.html", projectpath=projectpath)
 
 app.run(debug=True)
--- drink_program.py
+++ drink_program.py
@@ -1,16 +1,70 @@
 import requests
 import random
 
+class Drinks:
+    def __init__(self) -> None:
+        self._drinker_usortert = []
+        self._drinker_sortert = []
+        self._ingredienser = []
+        self._drinker_sortert_dic = {}
+        self._bilder_og_ingredienser = []
+        for i in range(4):
+            link = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+            o = requests.get(link)
+            self._drinker_usortert.append(o.json())
+        for i in self._drinker_usortert:
+            self._drinker_sortert.append(i["drinks"][0]["strDrink"])
+            self._bilder.append(i["drinks"][0]["strDrinkThumb"])
+            midlertidig_ingredienser_drink = []
+            for a in range(15):
+                a += 1
+                if not i["drinks"][0]["strIngredient"+str(a)] == None:
+                        midlertidig_ingredienser_drink.append(i["drinks"][0]["strIngredient"+str(a)])
+            self._drinker_sortert_dic[i["drinks"][0]["strDrink"]] = midlertidig_ingredienser_drink
+            self._ingredienser.append(midlertidig_ingredienser_drink)
+
+    def hent_drinker(self):
+        return list(self._drinker_sortert_dic)
+    
+    def hent_bilder(self):
+        return self._bilder
+    
+    def hent_ingredienser_til_drink(self):
+        global randomtall
+        global drinkListe
+        drinkListe = list(self._drinker_sortert_dic.keys())
+        randomtall = random.randint(0, len(drinkListe) - 1)
+        randomDrink = drinkListe[randomtall]
+        return self._drinker_sortert_dic[randomDrink]
+    
+    def hent_riktig_drink(self):
+        global randomtall
+        global drinkListe
+        return drinkListe[randomtall]
+    
+    def sjekk_vinn(self):
+        self.alle_drinker = self.hent_drinker()
+        print(self.alle_drinker)
+        self.gjett_drink = input(f"Hvilken drink tror du bruker ingrediensene {self.hent_ingredienser_til_drink()}?\n")
+        if self.alle_drinker[int(self.gjett_drink)] == self.hent_riktig_drink():
+            print(self.hent_riktig_drink())
+        else:
+            print(f"Feil svar, det var {self.hent_riktig_drink()}")
+
+# print(hent_ingredienser_til_drink())
+
 drinker_usortert = []
 drinker_sortert = []
 ingredienser = []
 drinker_sortert_dic = {}
+bilder = []
 for i in range(4):
     link = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
     o = requests.get(link)
     drinker_usortert.append(o.json())
 for i in drinker_usortert:
     drinker_sortert.append(i["drinks"][0]["strDrink"])
+    bilder.append(i["drinks"][0]["strDrinkThumb"])
     midlertidig_ingredienser_drink = []
     for a in range(15):
         a += 1
@@ -19,34 +73,5 @@ for i in drinker_usortert:
     drinker_sortert_dic[i["drinks"][0]["strDrink"]] = midlertidig_ingredienser_drink
     ingredienser.append(midlertidig_ingredienser_drink)
 
-def hent_drinker():
-    return list(drinker_sortert_dic)
-
-def hent_ingredienser_til_drink():
-    global randomtall
-    global drinkListe
-
-    drinkListe = list(drinker_sortert_dic.keys())
-    randomtall = random.randint(0, len(drinkListe) - 1)
-    randomDrink = drinkListe[randomtall]
-
-    return drinker_sortert_dic[randomDrink]
-
-def hent_riktig_drink():
-    global randomtall
-    global drinkListe
-
-    return drinkListe[randomtall]
-
-def sjekk_vinn():
-    alle_drinker = hent_drinker()
-    print(alle_drinker)
-    gjett_drink = input(f"Hvilken drink tror du bruker ingrediensene {hent_ingredienser_til_drink()}?\n")
-    if alle_drinker[int(gjett_drink)] == hent_riktig_drink():
-        print(f"Riktig svar!")
-        print(hent_riktig_drink())
-    else:
-        print(f"Feil svar, det var {hent_riktig_drink()}")
-
-# print(hent_ingredienser_til_drink())
-print(sjekk_vinn())
+print(drinker_sortert_dic)
+                        
