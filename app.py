474ea8757446fdafe69bd2710ca3f96e5e5b0443
BernhardRy <bernhard.ry@gmail.com>
2023-03-30 13:57:13 +0200

    Fikset på drink programmet
    Fikset på drink programmet slik at det passer bedre med nettsiden

--- app.py
+++ app.py
@@ -10,13 +10,8 @@ def index():
     alle_drinker = drinks.hent_drinker()
     hent_ingredienser = drinks.hent_ingredienser_til_drink()
     riktig_drink = drinks.hent_riktig_drink()
-    bilder = drinks.hent_bilder()
+    hent_json = drinks.hent_json_drinker()
 
-    return render_template("index.html", alle_drinker=alle_drinker, riktig_drink=riktig_drink, hent_ingredienser=hent_ingredienser, bilder=bilder)
-
-@app.route('/handle_data', methods=['POST'])
-def handle_data():
-    projectpath = request.form['projectFilepath']
-    return render_template("index.html", projectpath=projectpath)
+    return render_template("index.html", alle_drinker=alle_drinker, riktig_drink=riktig_drink, hent_ingredienser=hent_ingredienser, hent_json=hent_json)
 
 app.run(debug=True)
--- drink_program.py
+++ drink_program.py
@@ -7,27 +7,29 @@ class Drinks:
         self._drinker_sortert = []
         self._ingredienser = []
         self._drinker_sortert_dic = {}
-        self._bilder_og_ingredienser = []
         for i in range(4):
             link = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
             o = requests.get(link)
             self._drinker_usortert.append(o.json())
         for i in self._drinker_usortert:
             self._drinker_sortert.append(i["drinks"][0]["strDrink"])
-            self._bilder.append(i["drinks"][0]["strDrinkThumb"])
+            bilder_og_ingredienser = []
             midlertidig_ingredienser_drink = []
+            bilder = []
+            bilder.append(i["drinks"][0]["strDrinkThumb"])
             for a in range(15):
                 a += 1
                 if not i["drinks"][0]["strIngredient"+str(a)] == None:
                         midlertidig_ingredienser_drink.append(i["drinks"][0]["strIngredient"+str(a)])
-            self._drinker_sortert_dic[i["drinks"][0]["strDrink"]] = midlertidig_ingredienser_drink
-            self._ingredienser.append(midlertidig_ingredienser_drink)
+            bilder_og_ingredienser.append(midlertidig_ingredienser_drink)
+            bilder_og_ingredienser.append(bilder)
+            self._drinker_sortert_dic[i["drinks"][0]["strDrink"]] = bilder_og_ingredienser
 
     def hent_drinker(self):
         return list(self._drinker_sortert_dic)
     
-    def hent_bilder(self):
-        return self._bilder
+    def hent_json_drinker(self):
+        return self._drinker_sortert_dic
     
     def hent_ingredienser_til_drink(self):
         global randomtall
@@ -35,7 +37,7 @@ class Drinks:
         drinkListe = list(self._drinker_sortert_dic.keys())
         randomtall = random.randint(0, len(drinkListe) - 1)
         randomDrink = drinkListe[randomtall]
-        return self._drinker_sortert_dic[randomDrink]
+        return self._drinker_sortert_dic[randomDrink][0]
     
     def hent_riktig_drink(self):
         global randomtall
@@ -53,25 +55,28 @@ class Drinks:
 
 # print(hent_ingredienser_til_drink())
 
-drinker_usortert = []
-drinker_sortert = []
-ingredienser = []
-drinker_sortert_dic = {}
-bilder = []
-for i in range(4):
-    link = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-    o = requests.get(link)
-    drinker_usortert.append(o.json())
-for i in drinker_usortert:
-    drinker_sortert.append(i["drinks"][0]["strDrink"])
-    bilder.append(i["drinks"][0]["strDrinkThumb"])
-    midlertidig_ingredienser_drink = []
-    for a in range(15):
-        a += 1
-        if not i["drinks"][0]["strIngredient"+str(a)] == None:
-                midlertidig_ingredienser_drink.append(i["drinks"][0]["strIngredient"+str(a)])
-    drinker_sortert_dic[i["drinks"][0]["strDrink"]] = midlertidig_ingredienser_drink
-    ingredienser.append(midlertidig_ingredienser_drink)
+# drinker_usortert = []
+# drinker_sortert = []
+# ingredienser = []
+# drinker_sortert_dic = {}
+# for i in range(4):
+#     link = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+#     o = requests.get(link)
+#     drinker_usortert.append(o.json())
+# for i in drinker_usortert:
+#     drinker_sortert.append(i["drinks"][0]["strDrink"])
+#     bilder_og_ingredienser = []
+#     midlertidig_ingredienser_drink = []
+#     bilder = []
+#     bilder.append(i["drinks"][0]["strDrinkThumb"])
+#     for a in range(15):
+#         a += 1
+#         if not i["drinks"][0]["strIngredient"+str(a)] == None:
+#                 midlertidig_ingredienser_drink.append(i["drinks"][0]["strIngredient"+str(a)])
+#     bilder_og_ingredienser.append(midlertidig_ingredienser_drink)
+#     bilder_og_ingredienser.append(bilder)
+#     drinker_sortert_dic[i["drinks"][0]["strDrink"]] = bilder_og_ingredienser
 
-print(drinker_sortert_dic)
+# print(drinker_sortert_dic)
+
                         
